0feede3f622eb3bf890c87edc57b104d371460e3
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-26 23:59:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+bands.sort((str1,str2)=>{
+	let s1 = str1.replace(/\b(?:a|A|the|The|an|An)\b/g,'').trim();
+	let s2 = str2.replace(/\b(?:a|A|the|The|an|An)\b/g,'').trim();
+	return s1.localeCompare(s2);
+});
+
+bands.map(value=>{
+	let parent=document.querySelector(".list");
+	let box = document.createElement("li");
+	box.innerHTML = `<h4>${value}</h4>`;
+	parent.appendChild(box);
+})
